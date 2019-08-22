684d357b9bab1aff7c2107ba07a72f1b2ca8d370
Thierry Oliveira <thierry_fernando@hotmail.com>
2019-08-22 17:40:44 -0300

    Finished first step

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,32 @@
+class CocktailsController < ApplicationController
+  before_action :find_cocktail, only: :show
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to @cocktail
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,36 @@
+class DosesController < ApplicationController
+  before_action :find_dose, only: [:show, :destroy]
+  before_action :find_cocktail, only: [:new, :create]
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to @cocktail
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose.destroy
+  end
+
+  private
+
+  def find_dose
+    @dose = Dose.find(params[:id])
+  end
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
+end
