f3e4c2bd989a96029442bb9f5dcd2288483892e1
Thierry Oliveira <thierry_fernando@hotmail.com>
2019-08-23 18:34:05 -0300

    Add validation photo

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -4,4 +4,5 @@ class Cocktail < ApplicationRecord
   has_many :ingredients, through: :doses
   validates :name, presence: true
   validates :name, uniqueness: true
+  validates :photo, presence: true
 end
