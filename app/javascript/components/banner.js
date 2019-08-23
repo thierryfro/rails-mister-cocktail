6dd0cf03c0ee190760d229f15dc22fc66c08d1e1
Thierry Oliveira <thierry_fernando@hotmail.com>
2019-08-22 22:50:41 -0300

    Add some CSS

---
+++ app/javascript/components/banner.js
@@ -0,0 +1,12 @@
+import Typed from 'typed.js';
+
+const initTyped = () => {
+  new Typed('#typed', {
+    strings: ["The Best drinks!", "PQP CONSEGUI","Tome todas!"],
+    typeSpeed: 50,
+    loop: true
+  });
+}
+
+
+export { initTyped };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,8 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 import 'bootstrap';
+import { initTyped } from '../components/banner'
+
 console.log('Hello World from Webpacker')
+
+initTyped();
