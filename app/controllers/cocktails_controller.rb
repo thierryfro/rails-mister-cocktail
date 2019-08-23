23c4af009b862c71a07c23e020379b780961a01e
Thierry Oliveira <thierry_fernando@hotmail.com>
2019-08-22 22:10:05 -0300

    Add some style

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,5 +1,5 @@
 class CocktailsController < ApplicationController
-  before_action :find_cocktail, only: :show
+  before_action :find_cocktail, only: [:show, :destroy]
   def index
     @cocktails = Cocktail.all
   end
@@ -18,6 +18,11 @@ class CocktailsController < ApplicationController
   end
 
   def show
+  end
+
+  def destroy
+    @cocktail.destroy
+    redirect_to root_path
   end
 
   private
